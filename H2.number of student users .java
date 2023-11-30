b6e588b408064f1929703656e162434a81a82bcd
Vaishnavi1193 <107098851+Vaishnavi1193@users.noreply.github.com>
2023-11-30 09:07:54 +0530

    Update H2.number of student users .java

--- H2.number of student users .java
+++ H2.number of student users .java
@@ -23,3 +23,13 @@ public class UserCount {
         System.out.println("Number of non-teaching staff: " + nonTeachingStaff);
     }
 }
+OUTPUT:
+
+C:\Users\VAISHNAVI.R\OneDrive\Documents>javac UserCount.java
+
+C:\Users\VAISHNAVI.R\OneDrive\Documents>java UserCount
+Enter the total number of users: 540
+Enter the number of staff users: 450
+Number of student users: 90
+Number of staff users: 450
+Number of non-teaching staff: 150
